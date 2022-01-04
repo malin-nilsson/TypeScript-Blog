333c6b940b5234490ba3456c809449d6ca6ca551
Malin Nilsson <malinnilsson87@gmail.com>
2022-01-04 21:37:23 +0100

    Show modal when creating a new blog post

--- src/ts/createBlogPost.ts
+++ src/ts/createBlogPost.ts
@@ -15,7 +15,13 @@ import {
     BlogPost
 } from "./models/BlogPost";
 
+let modal: HTMLDivElement = document.getElementById("modal") as HTMLDivElement;
 
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 window.onload = function () {
     showBlogsInForm()
@@ -49,6 +55,11 @@ function createBlogPost() {
     if (titleValue || contentValue || blogNameValue) {
         blogPosts.push(new BlogPost(titleValue, contentValue, blogid, blogNameValue));
         localStorage.setItem("Blog posts", JSON.stringify(blogPosts));
+        modal.style.display = "block";
+        let closeModal: HTMLDivElement = document.querySelector(".close-wrapper");
+        closeModal.addEventListener("click", () => {
+            modal.style.display = "none";
+        })
 
     }
 
